44299450cc84d972c3b9e214070a6eb2adbc1247
Levente Löki <leventeloki@gmail.com>
2020-12-19 17:01:35 +0100

    Try deploy log

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,11 @@
 if [ "$GITHUB_EVENT_NAME" = "push" ]
 then
     echo "Deploy to Production..."
-    node_modules/.bin/netlify deploy --prod
+    node_modules/.bin/netlify deploy --prod | tee logs/deploylog
 fi
 
 if [ "$GITHUB_EVENT_NAME" = "pull_request" ]
 then
     echo "Deploy Preview..."
-    node_modules/.bin/netlify deploy
+    node_modules/.bin/netlify deploy | tee logs/deploylog
 fi
