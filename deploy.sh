2131aa398e7536a7632abc24fe90efc0d5003930
Levente Löki <leventeloki@gmail.com>
2020-12-19 17:07:39 +0100

    Fixed deploy log file name

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,11 @@
 if [ "$GITHUB_EVENT_NAME" = "push" ]
 then
     echo "Deploy to Production..."
-    node_modules/.bin/netlify deploy --prod | tee logs/deploylog
+    node_modules/.bin/netlify deploy --prod | tee logs/deploy.log
 fi
 
 if [ "$GITHUB_EVENT_NAME" = "pull_request" ]
 then
     echo "Deploy Preview..."
-    node_modules/.bin/netlify deploy | tee logs/deploylog
+    node_modules/.bin/netlify deploy | tee logs/deploy.log
 fi
